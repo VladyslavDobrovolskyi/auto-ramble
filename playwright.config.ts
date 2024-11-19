5c59e7857cee087615ed4df7c9b86ba51a91114f
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2024-11-19 16:54:51 +0200

    Playwright Configuration

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+	testMatch: '**/*.{spec,specs,test,tests}.ts',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	use: {
+		trace: 'on-first-retry',
+	},
+
+	projects: [
+		{
+			name: 'todolist.james.am',
+			use: {
+				...devices['Desktop Chrome'],
+				baseURL: 'https://microsoftedge.github.io/Demos/demo-to-do/',
+				viewport: { width: 1024, height: 768 },
+			},
+			testDir: './microsoftedge.github.io',
+		},
+	],
+})
