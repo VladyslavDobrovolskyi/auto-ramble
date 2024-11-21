d4d1d40548f325539cd7165778c665a8aa2ed8a7
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2024-11-21 17:59:46 +0200

    Base Page Object

--- the-internet.herokuapp.com/utils/poms/page.ts
+++ the-internet.herokuapp.com/utils/poms/page.ts
@@ -2,6 +2,9 @@ import { browser } from '@wdio/globals'
 
 export default class Page {
 	public open(path: string) {
-		return browser.url(`https://the-internet.herokuapp.com/${path}/`)
+		return browser.url(`https://the-internet.herokuapp.com/${path}`)
+	}
+	public get do() {
+		return browser
 	}
 }
